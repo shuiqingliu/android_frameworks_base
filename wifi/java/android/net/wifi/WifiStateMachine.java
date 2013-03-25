0ed975d595a9220ea34868dfd831cfe70c2960f5
Quarx2k <agent00791@gmail.com>
2013-03-25 18:18:23 +0700

    Revert "wifi: add rx filter init to fix broken unicast/broadcast (tiwlan)"
    This reverts commit a4866f427395125291da528acd14857144e139e3.

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -2766,11 +2766,6 @@ public class WifiStateMachine extends StateMachine {
             /* initialize network state */
             setNetworkDetailedState(DetailedState.DISCONNECTED);
 
-            if (Boolean.parseBoolean(SystemProperties.get("ro.wpa6_upnp_hack", ""))) {
-                /* set Rx filters to initial state */
-                mWifiNative.initializeRxFilters();
-            }
-
             /* Remove any filtering on Multicast v6 at start */
             mWifiNative.stopFilteringMulticastV6Packets();
 
